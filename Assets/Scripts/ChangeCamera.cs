18e28964cf0ad36679ca36bf134cf31b438bcd55
Dan Dumitrescu <dan.dumitrescu@live.com>
2020-01-19 18:31:20 -0800

    Added the ability to switch between cameras

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public Camera firstCamera;
+    public Camera secondCamera;
+
+    bool cam = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //firstCameraAudio = firstCamera.getComponent<AudioListener>();
+        //secondCameraAudio = secondCamera.getComponent<AudioListener>();
+        // CameraPositionChange(PlayerPrefs.GetInt("CameraPosition"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cam = !cam;
+           
+        }
+
+        firstCamera.gameObject.SetActive(cam);
+        secondCamera.gameObject.SetActive(!cam);     
+       
+    }
+
+  }
